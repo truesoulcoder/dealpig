643fdc0c85b8c3ba2390e6895f25116a7056becb
truesoulcoder <chrisphillips@truesoulpartners.com>
2025-04-28 12:04:23 -0500

    feat: Wrap AuthLayoutWrapper in Providers for improved context management

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
 import { AuthLayoutWrapper } from "@/components/auth/authLayout";
 import "@/styles/globals.css";
+import "@/styles/heroui.css";
+import "@/styles/leet.css";
+import { Providers } from "../providers";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AuthLayoutWrapper>{children}</AuthLayoutWrapper>;
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Providers>
+      <AuthLayoutWrapper>{children}</AuthLayoutWrapper>
+    </Providers>
+  );
 }
